47a9625a0877eb991069c2cb539576771387ffe0
Seo-yedam <dam04098@gmail.com>
2025-02-28 02:29:59 +0900

    date 수정 및 merge count 추가

--- yedam/preprocessing_code/merge/tokens_merge.py
+++ yedam/preprocessing_code/merge/tokens_merge.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import ast
 from tqdm import tqdm
 
 csv_files = [
@@ -11,11 +12,23 @@ csv_files = [
 
 merged_df = pd.DataFrame()
 
+# 일별 토큰 수 계산 함수
+def count_tokens(tagged_str):
+    try:
+        tokens = ast.literal_eval(tagged_str)
+        return len(tokens)
+    except Exception as e:
+        return 0
+
 for file in tqdm(csv_files, desc='Processing CSV files'):
     df = pd.read_csv(file)
-    selected_df = df[['date', 'tagged']]
+    df['token_count'] = df['tagged'].apply(count_tokens)
+    selected_df = df[['date', 'tagged', 'token_count']]
     merged_df = pd.concat([merged_df, selected_df], ignore_index=True)
 
-grouped_df = merged_df.groupby('date').agg({'tagged':lambda x: ', '.join(x.astype(str))})
+grouped_df = merged_df.groupby('date').agg({
+    'tagged': lambda x: ', '.join(x.astype(str)),
+    'token_count': 'sum'
+}).reset_index()
 
 grouped_df.to_csv('merged_tokens.csv')
