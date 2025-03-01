5a0e4c5b3bdc4890ac21871719fc60afd3635d46
Seo-yedam <dam04098@gmail.com>
2025-03-01 22:50:56 +0900

    ngram 코드 수정_품사 필터 추가

--- yedam/preprocessing/date_tag.py
+++ yedam/preprocessing/date_tag.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-df = pd.read_csv('tokenized_economy_results.csv')
+df = pd.read_csv('edaily_tokens.csv')
 
 df_selected = df[['date', 'tagged']]
 
-df_selected.to_csv('economy_docs.csv', index=False)
+df_selected.to_csv('edaily_docs.csv', index=False)
